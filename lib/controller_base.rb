3b9c56af2f6eff33b9a17a3b5c5d637552ba851f
App Academy Student <student@appacademy.io>
2016-08-10 16:25:31 -0700

    Added protect_from_forgery functionality

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -4,6 +4,7 @@ require 'erb'
 require_relative './session'
 require_relative './flash'
 require 'active_support/inflector'
+require 'byebug'
 
 class ControllerBase
   attr_reader :req, :res, :params
@@ -13,6 +14,21 @@ class ControllerBase
     @req = req
     @res = res
     @params = req.params.merge(params)
+  end
+
+  def self.protect_from_forgery
+    @@protect = true
+  end
+
+  def form_authenticity_token
+    @auth_token ||= SecureRandom.urlsafe_base64(32)
+    @res.set_cookie("authenticity_token", { path: "/", value: @auth_token })
+    @auth_token
+  end
+
+  def check_authenticity_token
+    cookie = @req.cookies["authenticity_token"] || nil
+    raise "Invalid authenticity token" unless cookie && cookie == params["authenticity_token"]
   end
 
   # Helper method to alias @already_built_response
@@ -58,6 +74,11 @@ class ControllerBase
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
     self.send(name)
+    if @@protect && @req.request_method != "GET"
+      check_authenticity_token
+    else
+      form_authenticity_token
+    end
     render (name) unless @already_built_response
   end
 
